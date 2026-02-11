863c62ecef06039dbe9e048abfc5634dd3ea4847
Tannu Ranga <tannuranga25@navgurukul.org>
2026-02-12 04:52:08 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,134 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choice1: "<script>",
+    choice2: "<javascript>",
+    choice3: "<js>",
+    choice4: "<scripting>",
+    answer: 1
+  },
+  {
+    question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choice1: "<script href='xxx.js'>",
+    choice2: "<script name='xxx.js'>",
+    choice3: "<script src='xxx.js'>",
+    choice4: "<script file='xxx.js'>",
+    answer: 3
+  },
+  {
+    question: "How do you write 'Hello World' in an alert box?",
+    choice1: "msgBox('Hello World');",
+    choice2: "alertBox('Hello World');",
+    choice3: "msg('Hello World');",
+    choice4: "alert('Hello World');",
+    answer: 4
+  },
+  {
+    question: "How does a FOR loop start?",
+    choice1: "for (i = 0; i <= 5)",
+    choice2: "for (i = 0; i <= 5; i++)",
+    choice3: "for i = 1 to 5",
+    choice4: "for (i <= 5; i++)",
+    answer: 2
+  },
+  {
+    question: "How can you add a comment in a JavaScript?",
+    choice1: "'This is a comment",
+    choice2: "<!--This is a comment-->",
+    choice3: "//This is a comment",
+    choice4: "*This is a comment",
+    answer: 3
+  }
+];
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 5;
+
+const startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+};
+
+const getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    return window.location.assign("/end.html");
+  }
+
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+    
+    
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    
+    if (classToApply === "incorrect") {
+        
+        const correctChoice = choices.find(c => c.dataset["number"] == currentQuestion.answer);
+        correctChoice.parentElement.classList.add("correct");
+    }
+
+    setTimeout(() => {
+      
+      selectedChoice.parentElement.classList.remove(classToApply);
+      
+      
+      if (classToApply === "incorrect") {
+          const correctChoice = choices.find(c => c.dataset["number"] == currentQuestion.answer);
+          correctChoice.parentElement.classList.remove("correct");
+      }
+      
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+const incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+startGame();
